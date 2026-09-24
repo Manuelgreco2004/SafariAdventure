5192c42f90851beb0adb8f8119da2126908dc369
Manuelgreco2004 <Greco.M216053@stu.sanjac.edu>
2026-09-24 01:33:54 +0000

    Event instantiating in the works

--- Event.java
+++ Event.java
@@ -1,3 +1,14 @@
 public class Event {
-    
+    public static void main(String[] args) {
+        // Resources 
+        int points = 0;
+
+        String Berries = "You found some delicious blueberries!";
+
+        String Bird = "You have spotted a vulture!";
+
+        System.out.println("The event you came across with is: " + Berries);
+        System.out.println("How many points do you have now? " + points);
+
+    }
 }
